369bcb295410d3e77d6d735a09f3c10b003d1c0d
James Suescun <james.steven2318@gmail.com>
2022-05-20 16:18:10 -0400

    set up expressJS to manage nodeJS server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+// const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('dotenv').config();
+// require('./config/database');
+
+const app = express();
+
+app.use(logger('dev'));
+
+// app.use('/api/users', require('./routes/api/users'));
+
+const port = process.env.PORT || 1010;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
